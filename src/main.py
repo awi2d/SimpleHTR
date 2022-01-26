29a51279791a24282acab8216429bc5ff6055b1d
awi2d <57232634+awi2d@users.noreply.github.com>
2022-01-26 21:07:05 +0100

    changed default to train on lines without wordbeamsearch

--- src/main.py
+++ src/main.py
@@ -139,7 +139,7 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--mode', choices=['train', 'validate', 'infer'], default='train')
-    parser.add_argument('--decoder', choices=['bestpath', 'beamsearch', 'wordbeamsearch'], default='wordbeamsearch')
+    parser.add_argument('--decoder', choices=['bestpath', 'beamsearch', 'wordbeamsearch'], default='bestpath')  # change Settings.python interpreter to wordbeamsearch to use wordbeamsearch.
     parser.add_argument('--batch_size', help='Batch size.', type=int, default=100)
     parser.add_argument('--data_dir', help='Directory containing IAM dataset.', type=Path, required=False, default='../trainingDataset')
     parser.add_argument('--fast', help='Load samples from LMDB.', action='store_true')
